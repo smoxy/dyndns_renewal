777ba8d6df17ef7def16b56427274592b3811f1b
smoxy <smoxy@hotmail.it>
2022-09-20 00:09:59 +0200

    V.0.2 command line option

--- dyndns_renewal.py
+++ dyndns_renewal.py
@@ -10,6 +10,8 @@ import datetime
 import os
 import tkinter as tk
 import configparser
+import getopt
+import sys
 
 from time import sleep
 
@@ -24,17 +26,36 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+V="0.2"
+shortopts = '''hlciV''' #if after the letter there is ':' so the argument is required
+longopts = ["help", "headless", "cookiesless", "noimage", "version"] #if after name variable ther is '=' so the argument is required
 
-done = False
-
-def create_db(cur):
+def create_db(con):
+    cur = con.cursor()
     cur.execute('''CREATE TABLE renews(
     datetime NUMERIC PRIMARY KEY,
     action TEXT
     );''')
 
+def help():
+    """
+    print help window
+    """
+    print(f"""dyndns-renewal {V}
+(C) 2022-2023 Simone Flavio Paris.
+Released under the GNU GPLv2.
 
-def getDriver(headless=True):
+    -h --help               Print this help screen
+    -l --headless           Use this script in headless mode
+    -c --cookiesless        Try to login preferring user and password avery time inputting instead of automatic cookies login
+    -i --noimage            Reduce bandwidth waste disabling image loading
+    -V --version            Print version info
+    """)
+def version():
+    print(f"dyndns-renewal {V}")
+
+
+def getDriver(headless, noimage):
     locale = "it"
     chrome_options = Options()
     chrome_options.add_argument(f"--lang={locale}")
@@ -44,9 +65,10 @@ def getDriver(headless=True):
         chrome_options.add_argument('--disable-gpu')
     chrome_prefs = {}
     #chrome_prefs["profile.managed_default_content_settings"] = {"javascript": 2}
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
-    chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
-    chrome_options.add_experimental_option('prefs', chrome_prefs)
+    if noimage:
+        chrome_prefs["profile.default_content_settings"] = {"images": 2}
+        chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
+        chrome_options.add_experimental_option('prefs', chrome_prefs)
     #chrome_options.add_argument("user-data-dir=selenium")
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--incognito")
@@ -56,7 +78,8 @@ def getDriver(headless=True):
     return driver
 
 
-def renew_service(driver):    
+def renew_service(driver, con):
+    cur = con.cursor()    
     #TODO click su renew service ogni domenica
     try:
         driver.find_element(By.CSS_SELECTOR, "button.ddns-button.clr-azzurro[value='confirmHost']").click()
@@ -73,16 +96,17 @@ def renew_service(driver):
         return False
         
 
-def login(config, headless=True, withCookies = False):
+def login(config, con, headless, cookiesless, noimage):
     config.read('./.settings/config.ini')
+    cur = con.cursor()
 
-    driver = getDriver(headless)
-    if withCookies:
+    driver = getDriver(headless, noimage)
+    if not cookiesless:
         try:
             cookies = pickle.load(open("./.settings/dyndns.pkl", "rb"))
         except FileNotFoundError:
             driver.close()
-            login(config, headless, withCookies=False)
+            login(config, headless, cookiesless=False)
         driver.get("https://dyndns.it/host-management/")
         driver.delete_all_cookies()
         for cookie in cookies:
@@ -119,8 +143,8 @@ def login(config, headless=True, withCookies = False):
                 pass
         except TimeoutException:
             driver.close()
-            if withCookies:
-                login(config, headless, withCookies=False)
+            if cookiesless:
+                login(config, headless, cookiesless=False)
             cur.execute('''INSERT INTO renews(datetime, action) VALUES(?,?);''', (datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), "ERROR: Confirm page Not Found"))
             con.commit()            
             raiseExceptions("ERROR: Confirm page Not Found")
@@ -133,7 +157,7 @@ def login(config, headless=True, withCookies = False):
     return driver
 
 
-def set_data(error=False):
+def set_data(config, headless, error=False):
     dir_bcp = os.getcwd()
     try:
         os.mkdir("./.settings")
@@ -144,39 +168,46 @@ def set_data(error=False):
     os.chdir("./.settings")
     config.read('./config.ini')
 
+    if headless:
+        if error:
+            print("Nome utente o password errati.")
+        user = input("USERNAME: ").strip()
+        pwd = input("PASSWORD: ").strip()
+        config["DEFAULT"]["user"] = user
+        config["DEFAULT"]["pwd"] = pwd
+    else:
+        # TK Windows
+        ws = tk.Tk(screenName="settings")
+        ws.title("settings")
+        ws.geometry('250x150')
 
-    # TK Windows
-    ws = tk.Tk(screenName="settings")
-    ws.title("settings")
-    ws.geometry('250x150')
+        if error:
+            tk.Label(text="Nome utente o password errati.\n").pack()
+            ws.geometry('250x200')
 
-    if error:
-        tk.Label(text="Nome utente o password errati.\n").pack()
-        ws.geometry('250x200')
+        def retrieve_input(obj, type):
+            inputValue=obj.get("1.0","end-1c")
+            if type == "user":
+                config["DEFAULT"]["user"] = str(inputValue)
+            else:
+                config["DEFAULT"]["pwd"] = str(inputValue)
 
-    def retrieve_input(obj, type):
-        inputValue=obj.get("1.0","end-1c")
-        if type == "user":
-            config["DEFAULT"]["user"] = str(inputValue)
-        else:
-            config["DEFAULT"]["pwd"] = str(inputValue)
+        userBox=tk.Text(ws, height=1, width=20)
+        pwdBox=tk.Text(ws, height=1, width=20)
 
-    userBox=tk.Text(ws, height=1, width=20)
-    pwdBox=tk.Text(ws, height=1, width=20)
-
-    userBoxCommit=tk.Button(ws, height=1, width=10, text="Commit", 
-                        command=lambda: retrieve_input(userBox, 'user'))
-    tk.Label(text="USERNAME:").pack()
-    userBox.pack()
-    userBoxCommit.pack()
-    pwdBoxCommit=tk.Button(ws, height=1, width=10, text="Commit", 
-                        command=lambda: retrieve_input(pwdBox,'pwd'))
-    tk.Label(text="PASSWORD:").pack()
-    pwdBox.pack()
-    pwdBoxCommit.pack()
+        userBoxCommit=tk.Button(ws, height=1, width=10, text="Commit", 
+                            command=lambda: retrieve_input(userBox, 'user'))
+        tk.Label(text="USERNAME:").pack()
+        userBox.pack()
+        userBoxCommit.pack()
+        pwdBoxCommit=tk.Button(ws, height=1, width=10, text="Commit", 
+                            command=lambda: retrieve_input(pwdBox,'pwd'))
+        tk.Label(text="PASSWORD:").pack()
+        pwdBox.pack()
+        pwdBoxCommit.pack()
 
 
-    ws.mainloop()
+        ws.mainloop()
     
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
@@ -184,20 +215,54 @@ def set_data(error=False):
     return
 
 
-if __name__ == "__main__":
+def main(headless, cookiesless, noimage):
     config = configparser.ConfigParser()
 
     isdb = os.path.isfile('dyndns.db')
     con = sqlite3.connect('dyndns.db')
-    cur = con.cursor()
     if not isdb:
-        create_db(cur, config)
+        create_db(con, config)
     if not os.path.isfile('./.settings/config.ini'):
-        set_data()
-    print(config)
+        set_data(config, headless)
+    
+    done = False
     while not done:
         try:
-            login(config, headless=True, withCookies=True)
+            login(config, con, headless, cookiesless, noimage)
             done = True
         except KeyError:
-            set_data(error=True)
+            set_data(config, headless, error=True)
+
+
+if __name__ == "__main__":
+    try:
+        try:
+            opts, args = getopt.getopt(args=sys.argv[1:], shortopts=shortopts, longopts=longopts)
+        except getopt.GetoptError as e:
+            print(f"{e}")
+            quit()
+
+        headless = False
+        cookiesless = False
+        noimage = False
+        doexit = False
+
+        for opt, arg in opts:
+            if opt in ['-h', '--help']:
+                help()
+                doexit = True
+            if opt in ['-l', '--headless']:
+                headless = True
+            if opt in ['-c', '--cookiesless']:
+                cookiesless = True
+            if opt in ['-i', '--noimage']:
+                noimage = True
+            if opt in ['-V', '--version']:
+                version()
+                doexit = True
+
+        if not doexit:
+            main(headless=headless, cookiesless=cookiesless, noimage=noimage)
+    except KeyboardInterrupt:
+        print("[i] Gracefully exit")
+        quit()
